b3b589d50e9e8e2345240137b90ddfea425b434e
levi5 <leviprudenciont@gmail.com>
2021-03-03 12:29:42 -0300

    feat: add TagContent model

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,7 @@
 class Content < ApplicationRecord
   belongs_to :
-  validates :title, :description, presence: true 
+  validates :title, :description, presence: true
+  
+  has_many :tag_contents
+  has_many :contents, through: :tag_contents
 end
