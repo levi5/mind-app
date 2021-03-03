d09bfc461197cd05d6ce5d03e1f8bcf2f371ca36
levi5 <leviprudenciont@gmail.com>
2021-03-03 12:40:58 -0300

    bug: add dependent destroy Tag and Content

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,6 +2,6 @@ class Content < ApplicationRecord
   belongs_to :
   validates :title, :description, presence: true
   
-  has_many :tag_contents
+  has_many :tag_contents, dependent: :destroy
   has_many :contents, through: :tag_contents
 end
